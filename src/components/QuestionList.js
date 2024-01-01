6383321eaeb86d914df4df250c11f982256eaf76
Elise Erickson <elisece717@gmail.com>
2024-01-01 09:34:47 -0500

    fetches questions from local server

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,22 @@
 import React from "react";
+import QuestionItem from './QuestionItem'
 
 function QuestionList() {
+
+  const [questions, setQuestions] = React.useState([])
+
+  React.useEffect( () => {
+    fetch("http://localhost:4000/questions")
+    .then( res => res.json())
+    .then( data => setQuestions(data))
+  }, [])
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{
+        questions.map( ques => <QuestionItem question={ques}/>)
+        }</ul>
     </section>
   );
 }
